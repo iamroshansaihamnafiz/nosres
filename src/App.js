76558bea8e40c21ea9bbda0dfd7c99432da9eac7
Roshan SaiHam Nafiz <118830871+iamroshansaihamnafiz@users.noreply.github.com>
2024-03-09 12:18:42 +0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Layouts from "./Components/Layouts";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Register from "./Pages/Register";
